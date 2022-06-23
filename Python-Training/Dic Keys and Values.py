b55fa65662973ab7c6788e9e7bde2f99c2f955ab
merhassa <merhassa@cisco.com>
2022-06-23 10:36:02 +0530

    program added

---
+++ Python-Training/Dic Keys and Values.py
@@ -0,0 +1,15 @@
+d1 = {"a":1,"b":2,"c":3}
+d2 = {"d":4,"e":5,"f":6}
+d3 = d1 | d2
+Keys = []
+values = []
+
+print("d3: ",d3)
+for i in d3:
+    Keys.append(i)
+    values.append(d3[i])
+
+print('Dictionary Keys: {}'.format(Keys))
+print('Dictionary values: {}'.format(values))
+
+
