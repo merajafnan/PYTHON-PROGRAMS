88cf5afbeb3c7c5e37474024906f0d1785360fed
merhassa <merhassa@cisco.com>
2022-06-23 11:55:39 +0530

    program added

---
+++ Python-Training/String Modification.py
@@ -0,0 +1,9 @@
+str1 = 'jump zoo zoo jump Taxi Aeroplane Taxi zoo'
+str2 = 'fly up till next universe'
+
+# Create string with only unique words
+# add 'saturn' at end of str1
+# add 'jupiter' after 'jump' of str1
+# delete 'aeroplane' of str1
+# merge Str1 & str2 string with '_'
+# split after merging the string
