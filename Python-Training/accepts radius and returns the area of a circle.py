12e346d4d569df1026076e66ff8bb7d7f6a74270
merhassa <merhassa@cisco.com>
2022-07-01 11:40:35 +0530

    program added

--- Python-Training/accepts radius and returns the area of a circle.py
+++ Python-Training/accepts radius and returns the area of a circle.py
@@ -4,3 +4,8 @@ def area(pi,r):
 r = int(input("Enter the radius: \n"))
 pi = 22/7
 print("Area of Circle is: {:.3f}".format(area(pi,r)))
+
+# Output:
+# Enter the radius:
+# 6
+# Area of Circle is: 113.143
