650f6c03d20e9c78876d987961f1bee9039ae2f8
merhassa <merhassa@cisco.com>
2022-07-19 14:33:10 +0530

    program aadded

--- Python-Training/Function - Remove all occurrences of a specific item.py
+++ Python-Training/Function - Remove all occurrences of a specific item.py
@@ -7,3 +7,7 @@ lst = ['test','boot','swim','test','test','test','test','find','test']
 item = 'test'
 remove_ = remove_(lst,item)
 print("New list after removal of perticulat item : {}".format(remove_))
+
+# Output:
+#
+# New list after removal of perticulat item : ['boot', 'swim', 'find']
---
+++ Python-Training/Lambda - Misc.py
@@ -0,0 +1,8 @@
+sum = lambda x,y: x+y
+print(sum(3,5))
+
+double = lambda double: double*2
+print(double(4))
+
+check = lambda check: True if check.isupper() else False
+print(check('TEST'))
