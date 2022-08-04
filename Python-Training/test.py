9d299649bbdbfcf73492f7d7d7d0db8078551742
merhassa <merhassa@cisco.com>
2022-08-04 15:21:26 +0530

    program aadded

--- Python-Training/test.py
+++ Python-Training/test.py
@@ -1,32 +1,3 @@
-str= '''PE1_ESP200x#sh ip int br
-Interface              IP-Address      OK? Method Status                Protocol
-Fo0/0/0                unassigned      YES unset  up                    up      
-Fo0/0/1                unassigned      YES unset  up                    up      
-Te0/1/0                99.1.1.1        YES NVRAM  up                    up      
-Te0/1/1                unassigned      YES unset  down                  down    
-Te0/1/2                77.1.1.1        YES NVRAM  up                    up      
-Te0/1/3                unassigned      YES unset  down                  down 
-Te1/1/1.4001           102.1.1.2       YES NVRAM  up                    up      
-Te1/1/1.4002           102.1.2.2       YES NVRAM  up                    up      
-Te1/1/2                65.0.0.2        YES NVRAM  up                    up 
- '''
-
-str = str.split('\n')
-# print(str)
-str.pop()
-str.pop(0)
-# print(str)
-dic = {}
-for i in str:
-    z = i.split()
-    # print(z)
-    dic[z[0]] = z[5]
-print(dic)
-
-
-
-
-
-
-
-
+showdict={'group': {'bw6000': {'local': {'ks_role': 'Primary', 'ks_status': 'Alive', 'address': '15.15.15.1', 'next_sid_client_operation': 'NOTIFY', 're_initializing': 'No', 'kssid_overlap': 'No', 'sid_length_cfg': '24 bits (MEDIUM)', 'sid_length_used': '24 bits (MEDIUM)', 'current_kssid_inuse': 'none', 'kssids_assigned': 'none', 'kssids_used': 'none', 'old_kssids_used': 'none'}}, 'bw600-IPV6': {'local': {'ks_role': 'Primary', 'ks_status': 'Alive', 'address': '15.15.15.1', 'next_sid_client_operation': 'NOTIFY', 're_initializing': 'No', 'kssid_overlap': 'No', 'sid_length_cfg': '24 bits (MEDIUM)', 'sid_length_used': '24 bits (MEDIUM)', 'current_kssid_inuse': 'none', 'kssids_assigned': 'none', 'kssids_used': 'none', 'old_kssids_used': 'none'}}, 'bw6000_eft': {'local': {'ks_role': 'Primary', 'ks_status': 'Alive', 'address': '15.15.15.1', 'next_sid_client_operation': 'NOTIFY', 're_initializing': 'No', 'kssid_overlap': 'No', 'sid_length_cfg': '24 bits (MEDIUM)', 'sid_length_used': '24 bits (MEDIUM)', 'current_kssid_inuse': 'none', 'kssids_assigned': 'none', 'kssids_used': 'none', 'old_kssids_used': 'none'}}, 'bw600-IPV6_eft': {'local': {'ks_role': 'Primary', 'ks_status': 'Alive', 'address': '15.15.15.1', 'next_sid_client_operation': 'NOTIFY', 're_initializing': 'No', 'kssid_overlap': 'No', 'sid_length_cfg': '24 bits (MEDIUM)', 'sid_length_used': '24 bits (MEDIUM)', 'current_kssid_inuse': 'none', 'kssids_assigned': 'none', 'kssids_used': 'none', 'old_kssids_used': 'none'}}, 'bw-suiteB': {'local': {'ks_role': 'Primary', 'ks_status': 'Alive', 'address': '15.15.15.1', 'next_sid_client_operation': 'NOTIFY', 're_initializing': 'No', 'kssid_overlap': 'No', 'sid_length_cfg': '24 bits (MEDIUM)', 'sid_length_used': '24 bits (MEDIUM)', 'current_kssid_inuse': 'none', 'kssids_assigned': 'none', 'kssids_used': 'none', 'old_kssids_used': 'none'}}, 'bw-suiteB-v6': {'local': {'ks_role': 'Primary', 'ks_status': 'Alive', 'address': '15.15.15.1', 'next_sid_client_operation': 'NOTIFY', 're_initializing': 'No', 'kssid_overlap': 'No', 'sid_length_cfg': '24 bits (MEDIUM)', 'sid_length_used': '24 bits (MEDIUM)', 'current_kssid_inuse': 'none', 'kssids_assigned': 'none', 'kssids_used': 'none', 'old_kssids_used': 'none'}}}}
+dict_1 = list(showdict['group'].keys())
+print(dict_1)
