46e15538c560bfbb36a6ac23f06e9dcab439e076
merhassa <merhassa@cisco.com>
2022-08-08 15:15:00 +0530

    program aadded

--- Python-Training/Sort the below list in asending order.py
+++ Python-Training/Sort the below list in asending order.py
@@ -1,17 +1,24 @@
 l = [ 1,'b','c','A','D','Z','f',1,-1]
 
-# for i in range(len(l)):
-#     for j in range(len(l)-1):
-#         if ascii(l[j]) < ascii(l[j]):
-#             l[j], l[j+1] = l[j+1], l[j]
-# print(l)
+dig = []
+char = []
+z = 0
+for i in l:
+    z = str(i)
+    if z.isalpha():
+        char.append(i)
+    else:
+        dig.append(i)
 
-# print(z)
-#
-# for i in range(len(z)):
-#     for j in range(len(z)-1):
-#         if ord(str(z[j])) < ord(str(z[j+1])):
-#             z[j], z[j+1] = z[j+1], z[j]
-# print(z)
+for j in range(len(dig)):
+    for k in range(len(dig)-1):
+        if dig[k] > dig[k+1]:
+            dig[k], dig[k+1] = dig[k+1], dig[k]
+j, k = 0,0
+for j in range(len(char)):
+    for k in range(len(char)-1):
+        if char[k] > char[k+1]:
+            char[k], char[k+1] = char[k+1], char[k]
 
+print(dig+char)
 
