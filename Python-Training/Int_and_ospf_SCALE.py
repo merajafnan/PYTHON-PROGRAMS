9fbbae49ccec25802fd97509139fdbe3e92ac7a5
merhassa <merhassa@cisco.com>
2022-08-16 22:11:26 +0530

    program aadded

--- Python-Training/Int_and_ospf_SCALE.py
+++ Python-Training/Int_and_ospf_SCALE.py
@@ -9,20 +9,20 @@
 # with open('subint.txt','w') as fh:
 #     for i in range(1,255,4):
 #         if z < 10:
-#             fh.write("interface Gi1/1/1.{}\\nencapsulation dot1Q 100{}\\nip add 60.1.1.{} 255.255.255.252\\nno shut\\n".format(z,z,i))
+#             fh.write("interface Gi0/0/2.{}\\nencapsulation dot1Q 100{}\\nip add 63.1.1.{} 255.255.255.252\\nno shut\\n".format(z,z,i))
 #         if z >= 10 and z < 100:
-#             fh.write("interface Gi1/1/1.{}\\nencapsulation dot1Q 10{}\\nip add 60.1.1.{} 255.255.255.252\\nno shut\\n".format(z,z,i))
+#             fh.write("interface Gi0/0/2.{}\\nencapsulation dot1Q 10{}\\nip add 63.1.1.{} 255.255.255.252\\nno shut\\n".format(z,z,i))
 #         z += 1
 #     for i in range(1,255,4):
 #         if z >= 10 and z < 100:
-#             fh.write("interface Gi1/1/1.{}\\nencapsulation dot1Q 10{}\\nip add 60.1.2.{} 255.255.255.252\\nno shut\\n".format(z,z,i))
+#             fh.write("interface Gi0/0/2.{}\\nencapsulation dot1Q 10{}\\nip add 63.1.2.{} 255.255.255.252\\nno shut\\n".format(z,z,i))
 #         if z >= 100:
-#             fh.write("interface Gi1/1/1.{}\\nencapsulation dot1Q 1{}\\nip add 60.1.2.{} 255.255.255.252\\nno shut\\n".format(z,z,i))
+#             fh.write("interface Gi0/0/2.{}\\nencapsulation dot1Q 1{}\\nip add 63.1.2.{} 255.255.255.252\\nno shut\\n".format(z,z,i))
 #         z += 1
 #     for j in range(3,9):
 #         for i in range(1,255,4):
 #             if z >= 100:
-#                 fh.write("interface Gi1/1/1.{}\\nencapsulation dot1Q 1{}\\nip add 60.1.{}.{} 255.255.255.252\\nno shut\\n".format(z,z,j,i))
+#                 fh.write("interface Gi0/0/2.{}\\nencapsulation dot1Q 1{}\\nip add 63.1.{}.{} 255.255.255.252\\nno shut\\n".format(z,z,j,i))
 #             z += 1
 
 
@@ -32,7 +32,7 @@
 # with open('subint.txt','w') as fh:
 #     for i in range(1,255,4):
 #         if z < 10:
-#             fh.write("interface Gi1/1/1.{}\\nencapsulation dot1Q 100{}\\nip add 60.1.1.{} 255.255.255.252\\nno shut\\n".format(z,z,i))
+#             fh.write("interface Gi1/1/1.{}\nencapsulation dot1Q 100{}\nip add 60.1.1.{} 255.255.255.252\nno shut\n".format(z,z,i))
 #         z += 1
 
 
@@ -40,16 +40,16 @@
 
 # with open('subint.txt','w') as fh:
 #     for i in range(1,513):
-#         fh.write('no interface Gi1/1/1.{}\\n'.format(i))
+#         fh.write('no interface Gi0/0/2.{}\\n'.format(i))
 
 
 ##################################--OSPF--#############################################
 # 71.1.X.X
 
-with open('subint.txt','w') as fh:
-    for i in range(0,8):
-        for j in range(1,255,4):
-            fh.write('network 71.1.{}.{} 0.0.0.3 area 83\\n'.format(i,j))
+# with open('subint.txt','w') as fh:
+#     for i in range(0,8):
+#         for j in range(1,255,4):
+#             fh.write('network 72.1.{}.{} 0.0.0.3 area 83\\n'.format(i,j))
 
 ##################################--OSPF--#############################################
 # 71.1.X.X
@@ -60,6 +60,22 @@ with open('subint.txt','w') as fh:
 #             fh.write('no router ospf 1\\n'.format(i,j))
 
 
+##################################--SUB INTERFACE PER SYSTEM--#############################################
+
+z = 1
+with open('subint.txt','w') as fh:
+    for i in range(0,16):
+        for j in range(0,255):
+            fh.write('interface gigabitEthernet2/0/2.{}\\nencapsulation dot1Q {}\\nip address 21.{}.{}.1 255.255.255.0\\n'.format(z,z,i,j))
+            z += 1
+
+##################################--NO SUB INTERFACE PER SYSTEM--#############################################
+
+zz = 1
+with open('nosubint.txt','w') as fh1:
+        for jj in range(0,4080):
+            fh1.write('no interface gigabitEthernet2/0/2.{}\\n'.format(zz))
+            zz += 1
 
 
 
